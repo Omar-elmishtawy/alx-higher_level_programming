b4b006c4b99d6c726eadd3efb4ad6bc4fd08c010
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-10-30 17:39:49 -0700

    fasf

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,24 @@
 #include "lists.h"
 
+/**
+ * check_cycle - check_cycle
+ * @list: list
+ *
+ * Return: 0
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *pt1;
-	listint_t *pt2;
+	listint_t *pt1 = list;
+	listint_t *pt2 = list;
 
-	pt1 = malloc(sizeof(listint_t));
-	pt2 = malloc(sizeof(listint_t));
-
-	pt2 = list->next;
-	while (pt2)
+	while (pt2 != NULL && pt2->next != NULL)
 	{
-		pt1 = list;
-		while(pt1 != pt2)
-		{
-			if (pt2->next == pt1)
-			{
-				return (1);
-			}
-			pt1 = pt1->next;
-		}
-		pt2 = pt2->next;
+		pt1 = pt1->next;
+		pt2 = pt2->next->next;
+
+		if (pt1 == pt2)
+			return (1);
 	}
 	return (0);
 }
+
