57d885a1f9e82be19981ad7f99e0cf6c3d741405
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-10-30 17:22:28 -0700

    casas

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *pt1;
+	listint_t *pt2;
+
+	pt1 = malloc(sizeof(listint_t));
+	pt2 = malloc(sizeof(listint_t));
+
+	pt2 = list->next;
+	while (pt2)
+	{
+		pt1 = list;
+		while(pt1 != pt2)
+		{
+			if (pt2->next == pt1)
+				return (1);
+			pt1 = pt1->next;
+		}
+		pt2 = pt2->next;
+	}
+	return (0);
+}
