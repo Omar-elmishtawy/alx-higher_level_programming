1739855a74f98e1860f9c286b03cde784bbeaecc
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-10-30 18:05:46 -0700

    sad

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+try:
+    sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+except:
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
