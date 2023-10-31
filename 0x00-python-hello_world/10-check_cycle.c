202d70795b959c2bd05c642461f335dd5b170d14
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-10-30 17:30:09 -0700

    sdada

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,8 +5,6 @@ int check_cycle(listint_t *list)
 	listint_t *pt1;
 	listint_t *pt2;
 
-	if (!list)
-		return (NULL);
 	pt1 = malloc(sizeof(listint_t));
 	pt2 = malloc(sizeof(listint_t));
 
@@ -17,7 +15,9 @@ int check_cycle(listint_t *list)
 		while(pt1 != pt2)
 		{
 			if (pt2->next == pt1)
+			{
 				return (1);
+			}
 			pt1 = pt1->next;
 		}
 		pt2 = pt2->next;
