a39cc536ca39e25d51e1ac7a00317d64facec2c2
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-10-30 18:09:14 -0700

    dsadas

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import sys
-try:
-    sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-except:
-    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
