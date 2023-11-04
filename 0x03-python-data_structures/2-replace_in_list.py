e8400794a594468ef8a47ff9cdba15423ee731cd
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-11-04 15:19:37 -0700

    dsa

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
     """
-    replace the element at index idx
+    Replaces an element of a list at a specific position
     ...
 
-    parameters
+    Parameters
     ----------
-    my_list: list
-        list of integer
-    idx: integer
-        given infex
+    my_list : list
+        The list of elements
+    idx : integer
+        The given position
+    element : the new element
 
     Return:
-        The element when found
-        None if the index is negative
-        None if the index is larger than the list length
+        The original list if idx is negative or
+        if idx out of range (> len(my_list))
     """
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        for x in range(len(my_list)):
-            if x == idx:
-                my_list[x] = element
-                break
-        return my_list
+    my_list[idx] = element
+    return my_list
