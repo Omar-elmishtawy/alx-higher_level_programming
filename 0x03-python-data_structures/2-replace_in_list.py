532f42b99c66f55a6ecb576dcec1f416d70726ba
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-11-04 15:15:14 -0700

    das

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+    """
+    replace the element at index idx
+    ...
+
+    parameters
+    ----------
+    my_list: list
+        list of integer
+    idx: integer
+        given infex
+
+    Return:
+        The element when found
+        None if the index is negative
+        None if the index is larger than the list length
+    """
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
@@ -8,4 +24,3 @@ def replace_in_list(my_list, idx, element):
                 my_list[x] = element
                 break
         return my_list
-    
