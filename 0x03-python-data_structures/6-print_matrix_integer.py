4ac40648331bac71f659066102b2a4c2c95a8ac7
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-11-04 16:51:48 -0700

    das

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """
+    print matrix
+    ...
+
+    Parameters
+    ----------
+    matrix : list of lists
+        matrix nxm
+    Return:
+        None
     """
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
