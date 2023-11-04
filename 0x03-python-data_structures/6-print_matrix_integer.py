d6a511553aa864f6a2fef1dcdf6e36545b507005
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-11-04 16:57:27 -0700

    dasd

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -17,4 +17,4 @@ def print_matrix_integer(matrix=[[]]):
             print("{:d}".format(matrix[i][j]), end="")
             if j != (len(matrix) - 1):
                 print(" ", end="")
-        print()
+        print(end="")
