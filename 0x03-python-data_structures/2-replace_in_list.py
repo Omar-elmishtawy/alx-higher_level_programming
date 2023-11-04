e2194130daed1a5770d416238f6091bfba44ea88
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-11-04 15:23:23 -0700

    dasda

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -17,7 +17,6 @@ def replace_in_list(my_list, idx, element):
         if idx out of range (> len(my_list))
     """
 
-
     if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = element
