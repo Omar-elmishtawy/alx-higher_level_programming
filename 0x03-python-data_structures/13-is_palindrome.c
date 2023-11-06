e287ffff80546c73a3b89b5277a74043923aafd9
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-06 12:42:44 -0800

    dasd

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include "stdio.h"
+
+/* is_palindrome - check if it is palindrom
+ * @head : address of the head of the list
+ * Return : 0 if not palindrom 1 if palindrom
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *next = *head;
+	listint_t *prev = *head;
+	listint_t *temp = NULL;
+
+	while (next->next != NULL && next != NULL)
+	{
+		next = next->next->next;
+		prev = prev->next;
+	}
+	temp = prev;
+	next = temp->next;
+	prev = NULL;
+
+	while (next)
+	{
+		temp->next = prev;
+		prev = temp;
+		temp = next;
+		next = next->next;
+	}
+	temp->next = prev;
+
+	while (*head)
+	{
+		if ((*head)->n == temp->n)
+		{
+			*head = (*head)->next;
+			temp = temp->next;
+		}
+		else
+			break;
+	}
+	if (*head == NULL)
+		return (1);
+	return (0);
+}
