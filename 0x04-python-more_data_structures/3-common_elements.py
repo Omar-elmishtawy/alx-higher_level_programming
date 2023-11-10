b025710f160b0731805075a4dab15b20de41683a
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-10 05:08:41 -0800

    dasds

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,9 +3,3 @@
 def common_elements(set_1, set_2):
     common_elements = [i for i in set_1 for j in set_2 if i == j]
     return common_elements
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
-
