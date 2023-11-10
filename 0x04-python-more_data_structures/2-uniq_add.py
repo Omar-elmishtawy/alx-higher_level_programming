f78ad911a8d1c993218c7e43b4bda5ad0d69e11c
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-10 03:05:06 -0800

    dasd

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -8,7 +8,3 @@ def uniq_add(my_list=[]):
             result = result + i
             dic[i] = True
     return result
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
