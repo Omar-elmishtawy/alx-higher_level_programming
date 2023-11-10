2bae95ddea7aff4bdbc7f3b7672f256ed5d09853
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-09 16:59:56 -0800

    dsada

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_square = [[matrix[i][j]**2 for j in range(len(matrix[i]))]for i in range(len(matrix))]
+    matrix_square = [[matrix[i][j]**2 for j in range(len(matrix[i]))]\
+            for i in range(len(matrix))]
 
     return matrix_square
