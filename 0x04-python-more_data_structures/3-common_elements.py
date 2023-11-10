6a9edececea5c2a26bcc8dc7d93bd7e274e45840
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-10 04:23:39 -0800

    dsadas

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_elements = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                common_elements.append(i)
+    return common_elements
