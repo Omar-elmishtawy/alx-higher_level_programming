1743338ae6f433497e8de2c6ebbd44ef71453b82
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-10 04:50:19 -0800

    das

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,8 @@
 
 def common_elements(set_1, set_2):
     common_elements = []
-    for i in range(len(set_1)):
+    min_list = [len(set_1) if len(set_1) < len(set_2) else len(set_2)]
+    for i in range(len(min_list)):
         if list(set_1)[i] == list(set_2)[i]:
             common_elements.append(list(set_1)[i])
     return common_elements
