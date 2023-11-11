5e30077974c01ae7ef037112a665c45d0824ebac
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-11 10:31:56 -0800

    dsa

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/user/bin/python3
+
+def complex_delete(a_dictionary, value):
+    a_dic_cp = a_dictionary.copy()
+    for i in a_dic_cp.keys():
+        if a_dictionary[i] == value:
+            a_dictionary.pop(i)
+    return a_dictionary
