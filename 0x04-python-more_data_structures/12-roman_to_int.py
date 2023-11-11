e58f8eee3027609bdcabfe400042a642634a2a3a
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-10 18:54:48 -0800

    dsad

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    Roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    i = 0
+
+    if isinstance(roman_string, str) != True or roman_string == None:
+        return result
+
+    for i in range(len(roman_string) - 1):
+        if Roman.get(roman_string[i]) >= Roman.get(roman_string[i+1]):
+            result = result + Roman.get(roman_string[i])
+        else:
+            result = result - Roman.get(roman_string[i])
+    result = result + Roman.get(roman_string[len(roman_string) - 1])
+    return result
