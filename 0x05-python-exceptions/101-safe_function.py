91968cc05d214c0fa92c74bb3778853cdcca6efe
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-20 15:59:36 -0800

    ca

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+
+    try:
+        result = fct(*args)
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        result = None
+    return result
