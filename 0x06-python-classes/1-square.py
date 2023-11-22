403560d9ee2dc6f843b5900e911316b11f29a393
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-21 16:10:59 -0800

    csa

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """This is a class of square
+
+    Args:
+        size (int): size of square
+
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size):
+        self.__size = size
