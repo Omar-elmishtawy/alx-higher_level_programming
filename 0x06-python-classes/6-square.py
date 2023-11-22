8f56d2b2e5f59a1b32d8a701d575b1093b2e5bd6
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-21 16:58:13 -0800

    ca

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,15 +56,6 @@ class Square:
         return (self.__size)**2
 
     def my_print(self):
-        "print the square"
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print('#', end='')
-            print()
-        if self.__size == 0:
-            print()
-
-       def my_print(self):
         """print square with #"""
         if self.__size == 0:
             print()
