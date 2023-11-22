ef2f5a90445ccf0ea2d386aad3f35031a04bb6a3
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-21 16:17:45 -0800

    ca

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """This is a class of square
+
+    Args:
+        size (int): size of square
+
+    Attributes:
+        size (int): size of square
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return s*s
