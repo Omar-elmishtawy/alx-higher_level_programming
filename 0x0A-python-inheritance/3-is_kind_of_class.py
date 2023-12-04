9557c6a1f3ad2481a39e63efdab8951ba3db5dd4
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-04 11:28:44 -0800

    das

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""check if object is exactly instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if obj is same class"""
+    return (isinstance(obj, a_class))
