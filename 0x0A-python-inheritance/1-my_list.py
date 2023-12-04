63cd8d912a4488d0659b1c1fa33f3d75d2eed5e0
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-04 11:14:55 -0800

    das

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,9 @@ class named mylist
 """
 
 class MyList(list):
+    """Class inherts from list class"""
     def print_sorted(self):
+        """print list sorted ascendingly"""
         ls = self.copy()
         ls.sort()
         print(ls)
