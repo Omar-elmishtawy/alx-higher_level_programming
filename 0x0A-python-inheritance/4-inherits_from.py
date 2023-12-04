e1b01ba08bd819af83ebb2baa617cac66fc9cbc4
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-04 11:29:49 -0800

    das

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""check if object is exactly instance of class"""
+
+
+def inherits_from(obj, a_class):
+    """check if obj is same class"""
+    return (issubclass(obj, a_class))
