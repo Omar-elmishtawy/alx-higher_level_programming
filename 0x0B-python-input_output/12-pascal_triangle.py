28e87ed1035691d1feed13b90e45c28bdc25636e
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-05 14:19:38 -0800

    dsa

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""pascal"""
+import math
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return [[]]
+    ls = [[1]]
+    for i in range(1,n):
+        ls_tmp = []
+        for j in range(len(ls[i-1])+1):
+            if j == 0 or j == len(ls[i-1]):
+                ls_tmp.append(1)
+            else:
+                ls_tmp.append(ls[i-1][j-1] + ls[i-1][j])
+        ls.append(ls_tmp)
+    return ls
