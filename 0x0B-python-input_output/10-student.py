4edc5eaefd60f857097bcb3982ae1352d49a6547
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-05 12:36:46 -0800

    csa

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,16 +9,15 @@ class Student():
         self.last_name = lname
         self.age = age
 
-    def to_json(self, attrs=None):
-        if attrs is None:
-            return self.__dict__
-        elif for atr in attrs:
-            if type(atr) is not str:
+       def to_json(self, attr=None):
+        """
+            retrieves a dictionary representation of Student.
+            Args:
+                attr (list): attribute names that are to be retrieved.
+        """
+
+            if attr is not None:
+                res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+                return res
+            else:
                 return self.__dict__
-        else:
-            dic = {}
-            for i in range(len(attrs)):
-                for atr in self.__dict__:
-                    if attrs[i] == atr:
-                        dic[atr] = self.__dict__[atr]
-            return dic
