9da56c15f4edb1469e8937fa05d78fc4f88fd34d
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-04 16:19:08 -0800

    ca

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module for my int"""
+
+
+class MyInt(int):
+    """class for my _init"""
+    def __eq__(self, compare_to):
+        return self.real != compare_to
+
+    def __ne__(self, value):
+        return self.real == value
+        
