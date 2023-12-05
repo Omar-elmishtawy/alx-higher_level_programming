fbe067a2bff742f4cd920adff35fe1db3815e7c1
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-05 11:58:13 -0800

    csa

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,7 +3,7 @@
 import json
 
 
-def save_to_json_file(filename):
+def load_from_json_file(filename):
     """read file and print it to stdout"""
     with open(filename, "w") as f:
         obj = json.load(f)
