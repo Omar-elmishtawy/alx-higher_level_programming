d43a865cfb4da3de40f40af18654c6d001d43a80
root <omar.elmishtawy@gmail.com>
2023-12-10 16:13:18 -0800

    dsa

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,64 @@
 #!/usr/bin/python3
 """Base class"""
+import json
 
 
 class Base:
     """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id_ is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to json file"""
+        filename = cls.__name__ + ".json"
+        text = []
+        if list_objs is not None:
+            for lst in list_objs:
+                text.append(lst.to_dictionary())
+        with open(filename, mode="w", encoding="utf-8") as f:
+            return f.write(Base.to_json_string(text))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create a new object from dictionary"""
+        if cls.__name__ == "Rectangle":
+            new = cls(10, 10)
+        elif cls.__name__ == "Square":
+            new = cls(10, 10)
+        new.update(**dictionary)
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        """
+            returns a list of instances
+        """
+        fname = cls.__name__ + ".json"
+
+        try:
+            with open(fname, encoding='utf8') as jfile:
+                content = cls.from_json_string(jfile.read())
+        except Exception as e:
+            return []
+
+        instances = []
+
+        for instance in content:
+            temp = cls.create(**instance)
+            instances.append(temp)
+
+        return instances
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,10 @@ class Rectangle(Base):
             self.y = args[4]
         except Exception as e:
             pass
+
+    def to_dictionary(self):
+        return {'id': self.id
+                'width': self.width
+                'height': self.height
+                'x', self.x
+                'y', self.y}
