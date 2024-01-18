438212c7e83988b61ef2f244976335ce08575c19
root <omar.elmishtawy@gmail.com>
2024-01-17 16:04:01 -0800

    ca

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,9 @@
 -- mklsda
 SELECT t.`title`, g.`name`
-  FROM `tv_shows` AS tv
-       LEFT JOIN `tv_show_genres` AS gen
-       ON tv.`id` = gen.`show_id`
-       LEFT JOIN `tv_genres` AS tvg
-       ON gen.`genre_id` = tvg.`id`
- ORDER BY tv.`title`, tvg.`name`;
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
