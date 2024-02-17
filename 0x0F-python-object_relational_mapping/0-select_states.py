86f76f24543165bd7f0071a29739b0210f6cf5aa
root <omar.elmishtawy@gmail.com>
2024-02-17 15:59:30 -0800

    das

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,14 @@
 
 """Execute sql queries using MySQLdb"""
 import MySQLdb
-import sys 
+import sys
 
-if __name__ == 'main'
-    db = MySQLdb.connect(user=sys.argv[0], passwd=sys.argv[1], db=sys.argv[2], port=3306)
-    c=db.cursor()
+if __name__ == 'main':
+    db = MySQLdb.connect(user=sys.argv[0],
+                         passwd=sys.argv[1],
+                         db=sys.argv[2],
+                         port=3306)
+    c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY if ASC")
     states = c.fetchall()
 
