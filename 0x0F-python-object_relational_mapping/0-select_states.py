516965b2eb1fbaccc04717f97bee1cee8b9dfddf
root <omar.elmishtawy@gmail.com>
2024-02-17 16:02:18 -0800

    das

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,10 @@ if __name__ == 'main':
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
-                         port=3306)
+                         port=3306,
+                         host='localhost')
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY if ASC")
+    c.execute("SELECT * FROM states ORDER BY id ASC")
     states = c.fetchall()
 
     for state in states:
