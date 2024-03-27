b074ba44484d1e2812ac5ad71230f35b6a67cffa
root <omar.elmishtawy@gmail.com>
2024-03-26 19:18:06 -0700

    ca

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const url_path = process.argv[2];
-const fs = require('fs');
 const request = require('request');
+const url = process.argv[2];
 
-request(url_path, function (error, response, body) {
+request(url, function (err, response, body) {
   if (err) {
-	  console.log(err);
+    console.log(err);
   } else {
-  	console.log('code: ',response.statusCode); // Print the response status code if a response was received
+    console.log('code: ' + response.statusCode);
   }
 });
