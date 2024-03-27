5559e9f5238c44bad74c87a39da7d580cd546d39
root <omar.elmishtawy@gmail.com>
2024-03-26 20:16:45 -0700

    csa

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,22 +7,21 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-	  const parsedBody = JSON.parse(body);
-	  const dict = {};
-	  var count = 0;
-	  var user_id = parsedBody[0].userId;
-	  for (let i = 0; i < parsedBody.length; i++){
-	     if (user_id === parsedBody[i].userId && parsedBody[i].completed) {
-		     count = count + 1;
-	     } else if (user_id !== parsedBody[i].userId){
-		     dict[user_id] = count;
-		     user_id = parsedBody[i].userId;
-		     count = 0;
-		     i = i - 1;
-	     }
-	  }
-	  dict[user_id] = count;
-	  console.log(dict);
+    const parsedBody = JSON.parse(body);
+    const dict = {};
+    let count = 0;
+    let userId = parsedBody[0].userId;
+    for (let i = 0; i < parsedBody.length; i++) {
+      if (userId === parsedBody[i].userId && parsedBody[i].completed) {
+        count = count + 1;
+      } else if (userId !== parsedBody[i].userId) {
+        dict[userId] = count;
+        userId = parsedBody[i].userId;
+        count = 0;
+        i = i - 1;
+      }
+    }
+    dict[userId] = count;
+    console.log(dict);
   }
 });
-	    
