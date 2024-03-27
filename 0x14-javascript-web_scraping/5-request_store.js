e5bc5530af65722131d5acee3cee56a68dea21bf
root <omar.elmishtawy@gmail.com>
2024-03-26 19:33:12 -0700

    dsa

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
-const api = process.argv[2];
-const filepath = process.argv[3];
-
-request({ url: api, methods: 'GET' }, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filepath, body, function (err) {
-	    if (err) {
-		    console.log(err);
-	    }
-  }
-});
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
